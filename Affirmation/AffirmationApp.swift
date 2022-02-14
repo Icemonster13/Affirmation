c1b97bc180876320ccb805a928fc29a77f584b82
Michael Pascucci <mdpascucci@live.com>
2022-02-14 07:30:58 -0500

    Initial Commit

---
+++ Affirmation/AffirmationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationApp.swift
+//  Affirmation
+//
+//  Created by Michael & Diana Pascucci on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct AffirmationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
